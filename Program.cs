5078bcc55bf534eec77dc2ae353199ec22d6b110
UTI <idats@localhost.localdomain>
2026-04-21 13:03:03 +0200

    modifying the scope

--- Program.cs
+++ Program.cs
@@ -7,62 +7,69 @@ using Microsoft.Extensions.Hosting;
 using WebAPI.Data.Repositories;
 
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Configuration
-builder.Configuration.Sources.Clear();
-
-IHostEnvironment env = builder.Environment;
-
-builder.Configuration
-    .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-    .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
-
-AppConfiguration options = new();
-builder.Configuration.GetSection(nameof(AppConfiguration))
-    .Bind(options);
-
-builder.Services.AddSingleton<IAppConfiguration>(sp => sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<AppConfiguration>>().Value);
-
-//DB
-builder.Services.AddSingleton<Dbms>();
-builder.Services.AddSingleton<IUVRepository, UVRepository>();
-
-builder.Services.AddSingleton<IAppTimer, AppTimer>();
-builder.Services.AddSingleton<IMQTTBroker, MQTTBroker>();
-builder.Services.AddSingleton<ISubscription, Subscription>();
-builder.Services.AddSingleton<IAuthClient, AuthClient>();
-
-
-builder.Services.AddSingleton<ICollectDataFromUV, CollectDataFromUV>();
-builder.Services.AddSingleton<CollectDataFromUVFacade>();
-
-builder.Services.AddOpenApi(options =>
+public partial class Program
 {
-    // Specify the OpenAPI version to use
-    options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
 
-});
+        // Configuration
+        builder.Configuration.Sources.Clear();
+
+        IHostEnvironment env = builder.Environment;
+
+        builder.Configuration
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", true, true);
+
+        AppConfiguration options = new();
+        builder.Configuration.GetSection(nameof(AppConfiguration))
+            .Bind(options);
+
+        builder.Services.AddSingleton<IAppConfiguration>(sp => sp.GetRequiredService<Microsoft.Extensions.Options.IOptions<AppConfiguration>>().Value);
+
+        //DB
+        builder.Services.AddSingleton<Dbms>();
+        builder.Services.AddSingleton<IUVRepository, UVRepository>();
+
+        builder.Services.AddSingleton<IAppTimer, AppTimer>();
+        builder.Services.AddSingleton<IMQTTBroker, MQTTBroker>();
+        builder.Services.AddSingleton<ISubscription, Subscription>();
+        builder.Services.AddSingleton<IAuthClient, AuthClient>();
+
+
+        builder.Services.AddSingleton<ICollectDataFromUV, CollectDataFromUV>();
+        builder.Services.AddSingleton<CollectDataFromUVFacade>();
+
+        builder.Services.AddOpenApi(options =>
+        {
+            // Specify the OpenAPI version to use
+            options.OpenApiVersion = Microsoft.OpenApi.OpenApiSpecVersion.OpenApi3_0;
+
+        });
 
 
 
-var app = builder.Build();
+        var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
 
-    app.MapOpenApi();
+            app.MapOpenApi();
+        }
+
+
+        app.MapGet("/", () => "Hello, Welcome to FishGuard AI!");
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var facade = scope.ServiceProvider.GetRequiredService<CollectDataFromUVFacade>();
+        }
+
+
+        app.Run();
+    }
 }
-
-
-app.MapGet("/", () => "Hello, Welcome to FishGuard AI!");
-
-
-
-
-app.Run();
-
- 
